86bac077ce9dcfbe7de61b8979e236d2041dc838
Marcel <71490170+mnblski@users.noreply.github.com>
2024-01-20 09:49:06 +0000

    chore: add comments to bg class & change naming

--- bgBlack.js
+++ bgBlack.js
@@ -1,8 +1,8 @@
-class BgBlack {
+class Background {
   constructor() {}
-
   //main background, frame, and rectangles that cover moving objects
   draw() {
+    // frame (cover moving objects outside of the game area)
     fill(0);
     noStroke();
     rect(-MAP_WIDTH, 0, MAP_WIDTH, MAP_HEIGHT);
@@ -19,6 +19,7 @@ class BgBlack {
     noStroke();
     rect(MAP_WIDTH, -400, 1000, MAP_HEIGHT + 2000);
 
+    // game aroun with a neon green border
     noFill();
     stroke("rgb(30,255,30)");
     strokeWeight(1);
