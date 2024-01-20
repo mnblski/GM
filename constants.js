fc69054fd90d4953705b9142358d8b9631690379
Marcel <71490170+mnblski@users.noreply.github.com>
2024-01-20 09:49:21 +0000

    chore: add browser check for safari

--- constants.js
+++ constants.js
@@ -1,5 +1,7 @@
 const MAP_WIDTH = 6000;
 const MAP_HEIGHT = 6000;
 
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+const isBrowserSupported = !isSafari;
 //let randomX = random(0, MAP_WIDTH);
 //let randomY = random(0, MAP_HEIGHT);
